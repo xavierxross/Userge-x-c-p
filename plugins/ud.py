362dce1d022f69e8451f8e2a6a6ad86a66dd9e83
xavierxross <68507234+xavierxross@users.noreply.github.com>
2021-05-28 10:01:03 +0530

    Create ud.py

---
+++ plugins/ud.py
@@ -0,0 +1,23 @@
+import asyncurban
+
+from userge import Message, userge
+
+
+@userge.on_cmd(
+    "ud",
+    about={
+        "header": "Searches Urban Dictionary for the query",
+        "usage": "{tr}ud [query] or [reply to message]",
+        "examples": ["{tr}ud userge"],
+    },
+)
+async def urban_dict(message: Message):
+    word = message.input_str or message.reply_to_message.text
+    urban = asyncurban.UrbanDictionary()
+    try:
+        mean = await urban.get_word(word)
+        await message.edit(
+            f"Text: {mean.word}**\n\nMeaning: {mean.definition}**\n\nExample: {mean.example}"
+        )
+    except asyncurban.WordNotFoundError:
+        await message.edit("No result found for " + word + "")
